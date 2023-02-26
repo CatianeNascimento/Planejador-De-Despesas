824d454ccadfdf0f927f8a820ca51c9ec9ce6774
CatianeNascimento <csnascimento07@gmail.com>
2023-02-26 10:01:10 -0300

    alterações na classe Despesas

--- src/teste/Despesa.java
+++ src/teste/Despesa.java
@@ -1,18 +1,18 @@
 package teste;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
 public class Despesa {
 	int posicao;
-
-	ArrayList<String> nomeDespesa = new ArrayList<String>();
-	ArrayList<String> tipoDespesa = new ArrayList<String>();
-	ArrayList<Double> valorDespesa = new ArrayList<Double>();
+	double soma;
+	static ArrayList<String> nomeDespesa = new ArrayList<String>();
+	static ArrayList<String> tipoDespesa = new ArrayList<String>();
+	static ArrayList<Double> valorDespesa = new ArrayList<Double>();
 
 	public Despesa() {
-
 	}
 
 	public ArrayList<String> getNomeDespesa() {
@@ -39,26 +39,26 @@ public class Despesa {
 		this.valorDespesa.add(valorDespesa);
 	}
 
-	// Marcos
+	// Marcos - Listar Despesas
 	public void visualizarDespesa() {
 		for (int i = 0; i < nomeDespesa.size(); i++) {
-			System.out.println("Nome da Despesa: " + nomeDespesa.get(i) + "\tValor Despesa: " + valorDespesa.get(i)
+			System.out.println("Nome da Despesa: " + nomeDespesa.get(i) + "\tValor Despesa: " + this.formataMoeda(valorDespesa.get(i))
 					+ "\tTipo Despesa: " + tipoDespesa.get(i));
 		}
 	}
 
-	// Joas
+	// Joas - Cadastrar Despesa
 	public void cadastrarDespesa(String nomeDespesa, int tipoDespesa, double valorDespesa) {
 		switch (tipoDespesa) {
 		case 1:
-			setTipoDespesa("Viagem");
+			setTipoDespesa("Contas");
 			setNomeDespesa(nomeDespesa);
 			setValorDespesa(valorDespesa);
 			System.out.println("Despesa cadastrada com sucesso! \n");
 			break;
 
 		case 2:
-			setTipoDespesa("Contas");
+			setTipoDespesa("Compras");
 			setNomeDespesa(nomeDespesa);
 			setValorDespesa(valorDespesa);
 			System.out.println("Despesa cadastrada com sucesso! \n");
@@ -72,7 +72,7 @@ public class Despesa {
 			break;
 
 		case 4:
-			setTipoDespesa("Compras");
+			setTipoDespesa("Viagem");
 			setNomeDespesa(nomeDespesa);
 			setValorDespesa(valorDespesa);
 			System.out.println("Despesa cadastrada com sucesso! \n");
@@ -91,12 +91,60 @@ public class Despesa {
 		}
 	}
 
-	// Thiago
+	// Thiago - Excluir Despesa
 	public void excluirDespesa() {
 
 	}
 
-	// Bia
+	// Bia - Editar Despesa
+	public void alterarDespesa(String novoNome, double novoValor, int novoTipo) {
+		this.nomeDespesa.remove(posicao);
+		this.valorDespesa.remove(posicao);
+		this.tipoDespesa.remove(posicao);
+
+		switch (novoTipo) {
+		case 1:
+			setTipoDespesa("Contas");
+			setNomeDespesa(novoNome);
+			setValorDespesa(novoValor);
+			System.out.println("Despesa alterada com sucesso! \n");
+			break;
+
+		case 2:
+			setTipoDespesa("Compras");
+			setNomeDespesa(novoNome);
+			setValorDespesa(novoValor);
+			System.out.println("Despesa alterada com sucesso! \n");
+			break;
+
+		case 3:
+			setTipoDespesa("Lazer");
+			setNomeDespesa(novoNome);
+			setValorDespesa(novoValor);
+			System.out.println("Despesa alterada com sucesso! \n");
+			break;
+
+		case 4:
+			setTipoDespesa("Viagem");
+			setNomeDespesa(novoNome);
+			setValorDespesa(novoValor);
+			System.out.println("Despesa alterada com sucesso! \n");
+			break;
+
+		case 5:
+			setTipoDespesa("Outros");
+			setNomeDespesa(novoNome);
+			setValorDespesa(novoValor);
+			System.out.println("Despesa alterada com sucesso! \n");
+			break;
+
+		default:
+			System.err.println("Opção inválida, entre novamente com os dados");
+			break;
+		}
+	}
+	
+	//Verifica se a despesa já está na lista
 	public boolean verificaDespesa(String nomeDespesa) {
 		if (this.nomeDespesa.contains(nomeDespesa)) {
 			posicao = this.nomeDespesa.indexOf(nomeDespesa);
@@ -106,51 +154,30 @@ public class Despesa {
 		}
 
 	}
-
-	public void alterarDespesa(String novoNome, double novoValor, int novoTipo) {
-		this.nomeDespesa.remove(posicao);
-		this.valorDespesa.remove(posicao);
-		this.tipoDespesa.remove(posicao);
-
-		switch (novoTipo) {
-		case 1:
-			setTipoDespesa("Viagem");
-			setNomeDespesa(novoNome);
-			setValorDespesa(novoValor);
-			System.out.println("Despesa alterada com sucesso! \n");
-			break;
-
-		case 2:
-			setTipoDespesa("Contas");
-			setNomeDespesa(novoNome);
-			setValorDespesa(novoValor);
-			System.out.println("Despesa alterada com sucesso! \n");
-			break;
-
-		case 3:
-			setTipoDespesa("Lazer");
-			setNomeDespesa(novoNome);
-			setValorDespesa(novoValor);
-			System.out.println("Despesa alterada com sucesso! \n");
-			break;
-
-		case 4:
-			setTipoDespesa("Compras");
-			setNomeDespesa(novoNome);
-			setValorDespesa(novoValor);
-			System.out.println("Despesa alterada com sucesso! \n");
-			break;
-
-		case 5:
-			setTipoDespesa("Outros");
-			setNomeDespesa(novoNome);
-			setValorDespesa(novoValor);
-			System.out.println("Despesa alterada com sucesso! \n");
-			break;
-
-		default:
-			System.err.println("Opção inválida, entre novamente com os dados");
-			break;
+	
+	//Verifica o valor final sobrante/faltante para pagamento das despesas
+	public void calculaDespesa(double montante) {
+		for(double i:valorDespesa) {
+			soma+=i;
 		}
+		
+		if(montante-soma>0) {
+			System.out.println("Você conseguirá pagar todas as despesas com sobra de "+formataMoeda(montante-soma));
+		}
+		else if(montante-soma<0){
+			System.out.println("Ficará faltando "+formataMoeda(montante-soma)+" para pagar suas despesas");
+		}
+		else {
+			System.out.println("Você tem o valor exato para pagar todas as despesas!!");
+		}
+	}
+	
+	//formatar valor double para moeda padrão (Brasil)
+	public String formataMoeda(double valor) {
+		NumberFormat valorFormatado = NumberFormat.getCurrencyInstance();
+		
+		valorFormatado.setMinimumFractionDigits(2);
+			String valorFinal = valorFormatado.format(valor);
+			return valorFinal;
 	}
 }
